619e14634b65831f32ed9da4aa9c4ccc7da0e96b
Henkho Dryza <simangahk@gmail.com>
2020-04-19 02:59:21 -0700

    fixed class name issue on jsx

--- src/components/svg-jsx/middleText.jsx
+++ src/components/svg-jsx/middleText.jsx
@@ -2,56 +2,59 @@ import React from 'react';
 
 export default (
   <g id="middle-text">
-    <text class="cls-4" transform="translate(288.6 43.66) rotate(-11.96)">
+    <text className="cls-4" transform="translate(288.6 43.66) rotate(-11.96)">
       S
     </text>
-    <text class="cls-4" transform="translate(309.86 38.96) rotate(-6.16)">
+    <text className="cls-4" transform="translate(309.86 38.96) rotate(-6.16)">
       O
     </text>
-    <text class="cls-4" transform="translate(338.27 35.95) rotate(-0.79)">
+    <text className="cls-4" transform="translate(338.27 35.95) rotate(-0.79)">
       C
     </text>
-    <text class="cls-4" transform="translate(363.53 35.78) rotate(2.77)">
+    <text className="cls-4" transform="translate(363.53 35.78) rotate(2.77)">
       I
     </text>
-    <text class="cls-4" transform="translate(374.95 36.02) rotate(6.75)">
+    <text className="cls-4" transform="translate(374.95 36.02) rotate(6.75)">
       A
     </text>
-    <text class="cls-4" transform="translate(401.8 39.27) rotate(12.33)">
+    <text className="cls-4" transform="translate(401.8 39.27) rotate(12.33)">
       L
     </text>
-    <text class="cls-4" transform="translate(244.2 92.82) rotate(-17.83)">
+    <text className="cls-4" transform="translate(244.2 92.82) rotate(-17.83)">
       D
     </text>
-    <text class="cls-4" transform="translate(270.9 84.56) rotate(-13.31)">
+    <text className="cls-4" transform="translate(270.9 84.56) rotate(-13.31)">
       I
     </text>
-    <text class="cls-4" transform="translate(282.1 81.74) rotate(-10.01)">
+    <text className="cls-4" transform="translate(282.1 81.74) rotate(-10.01)">
       S
     </text>
-    <text class="cls-4" transform="translate(303.61 77.92) rotate(-6.24)">
+    <text className="cls-4" transform="translate(303.61 77.92) rotate(-6.24)">
       T
     </text>
-    <text class="cls-4" transform="matrix(1, -0.04, 0.04, 1, 324.38, 75.51)">
+    <text
+      className="cls-4"
+      transform="matrix(1, -0.04, 0.04, 1, 324.38, 75.51)"
+    >
       A
     </text>
-    <text class="cls-4" transform="translate(351.7 74.28) rotate(1.54)">
+    <text className="cls-4" transform="translate(351.7 74.28) rotate(1.54)">
       N
     </text>
-    <text class="cls-4" transform="translate(378.96 75.01) rotate(5.6)">
+    <text className="cls-4" transform="translate(378.96 75.01) rotate(5.6)">
       C
     </text>
-    <text class="cls-4" transform="translate(404.21 77.65) rotate(8.71)">
+    <text className="cls-4" transform="translate(404.21 77.65) rotate(8.71)">
       I
     </text>
     <text
-      class="cls-4"
+      className="cls-4"
       transform="matrix(0.98, 0.21, -0.21, 0.98, 415.59, 79.1)"
     >
       N
     </text>
     <text
-      class="cls-4"
+      className="cls-4"
       transform="matrix(0.95, 0.31, -0.31, 0.95, 441.9, 84.76)"
     >
       G
